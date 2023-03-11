8e02a19165748f67822fa92bb1dad029e4f0a44d
ketanr <ketanr@trainee.nrifintech.com>
2023-03-11 08:15:25 +0530

    Upload New File

---
+++ src/main/java/com/canteen/service/EmailSenderService.java
@@ -0,0 +1,25 @@
+package com.canteen.service;
+
+import org.apache.logging.log4j.util.StringBuilderFormattable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService {
+
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	public void sendEmail(String toEmail,String toSubject,String Body)
+	{
+		SimpleMailMessage message=new SimpleMailMessage();
+		message.setFrom("ketanrajmath@gmail.com");
+		message.setTo(toEmail);
+		message.setSubject(toSubject);
+		message.setText(Body);
+		mailSender.send(message);
+		System.out.println("mail sent Successfully");
+	}
+}
