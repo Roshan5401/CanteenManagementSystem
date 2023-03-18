20d5970a478e230cec6ee6a3e1ca9251d052dcf7
shreyara <shreyara@trainee.nrifintech.com>
2023-03-18 17:08:43 +0530

    test files and icon

--- src/test/java/com/canteen/repository/GlobalRepositoryTest.java
+++ src/test/java/com/canteen/repository/GlobalRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.canteen.entities.GlobalEmployees;
@@ -12,17 +13,33 @@ import com.canteen.entities.GlobalEmployees;
 @SpringBootTest
 class GlobalRepositoryTest {
 
-	@Mock
-	private GlobalRepository globalrepository;
+	private GlobalRepository globalrepository = Mockito.mock(GlobalRepository.class);
+
+	private GlobalEmployees ge = new GlobalEmployees(1, "a@mail.com");
 
 	@Test
+
+	public void testSaveUser() {
+
+		Mockito.when(globalrepository.save(ge)).thenReturn(ge);
+
+		GlobalEmployees savedUser = globalrepository.save(ge);
+
+		assertEquals(ge, savedUser);
+
+	}
+
+	@Test
+
 	void testfindByEmail() {
 
-		GlobalEmployees ge = new GlobalEmployees(1, "a@mail.com");
-		globalrepository.save(ge);
+		String email = "a@mail.com";
 
-		GlobalEmployees ag = globalrepository.findByEmail("a@mail.com");
-		verify(globalrepository).findByEmail("a@mail.com");
+		Mockito.when(globalrepository.findByEmail(email)).thenReturn(ge);
+
+		GlobalEmployees actualUser = globalrepository.findByEmail(email);
+
+		assertEquals(ge, actualUser);
 
 	}
 
--- src/test/java/com/canteen/repository/MenuRepositoryTest.java
+++ src/test/java/com/canteen/repository/MenuRepositoryTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.canteen.entities.OrderEntity;
@@ -17,40 +18,54 @@ import com.canteen.entities.menuCanteen;
 @SpringBootTest
 class MenuRepositoryTest {
 
-	@Mock
-	private MenuRepository menurepository;
+	private MenuRepository menurepo=Mockito.mock(MenuRepository.class);
+
+	List<OrderEntity> orderentity = new ArrayList<OrderEntity>();
+
+	double price = 20;
+
+	String str = "2023-03-02";
+
+	Date dt = Date.valueOf(str);
+
+	int a = 1;
+
+	boolean b = (a == 1);
+
+	menuCanteen menu = new menuCanteen(1,"shreya",price,"Veg",dt,orderentity,b,"img");
 
 	@Test
-	void test() {
 
-		List<OrderEntity> orderentity = new ArrayList<OrderEntity>();
+	public void testSaveUser() {
 
-		menuCanteen menucanteen = new menuCanteen();
+	Mockito.when(menurepo.save(menu)).thenReturn(menu);
 
-		menucanteen.setID(1);
-		menucanteen.setName("shreya");
-		menucanteen.setPrice(23);
-		menucanteen.setType("Veg");
+	menuCanteen savedUser = menurepo.save(menu);
 
-		String str = "2023-03-02";
-		Date dt = Date.valueOf(str);
+	// System.out.println(savedUser);
 
-		menucanteen.setFoodServedDate(dt);
-		menucanteen.setListOrderEntities(orderentity);
-
-		int a = 1;
-		boolean b = (a == 1);
-		menucanteen.setEnable(b);
-		this.menurepository.save(menucanteen);
-
-		menuCanteen menu = menurepository.findById(1);
-		verify(menurepository).findById(1);
-
-		List<menuCanteen> menuc = new ArrayList<menuCanteen>();
-		menuc.add(menucanteen);
-
-		List<menuCanteen> m = menurepository.getFoodByEnable(b);
-		verify(menurepository).getFoodByEnable(b);
+	assertEquals(menu, savedUser);
 
 	}
-}
+
+	@Test
+
+	void test() {
+
+	//this.menurepository.save(menucanteen);
+
+	menuCanteen menuc = menurepo.findById(1);
+
+	verify(menurepo).findById(1);
+
+	List<menuCanteen> menucn = new ArrayList<menuCanteen>();
+
+	menucn.add(menu);
+
+	List<menuCanteen> m = menurepo.getFoodByEnable(b);
+
+	verify(menurepo).getFoodByEnable(b);
+
+	}
+
+	}
